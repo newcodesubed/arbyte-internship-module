6377111af7d47d7287c8060387443f7fa597b191
Subed Shah <subedshahxi@gmail.com>
2025-06-13 14:53:07 +0545

    added demo menu data

--- module1/3_conditional_rendering&lists/src/App.jsx
+++ module1/3_conditional_rendering&lists/src/App.jsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./Login";
+import MainMenu from "./MainMenu";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [user, setUser] = useState({ name: "", number: "" });
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <Router>
+      <div className="bg-yellow-400 min-h-screen text-white">
+        <Routes>
+          <Route path="/" element={<Login setUser={setUser} />} />
+          <Route path="/menu" element={<MainMenu user={user} />} />
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </Router>
+  );
 }
 
-export default App
+export default App;
