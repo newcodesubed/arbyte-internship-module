3cc318aa3838a9c5de032468bea016e2e12bd04d
Subed <subedshahxi@gmail.com>
2025-06-17 15:03:40 +0545

    feat: added joke

--- module1/4_useEffect_Side_Effects/src/components/JokePage.jsx
+++ module1/4_useEffect_Side_Effects/src/components/JokePage.jsx
@@ -1,5 +1,35 @@
-function JokePage() {
-  return <div>JOkePage</div>;
-}
+import { useEffect, useState } from "react";
 
+const JokePage = () => {
+  const [loading, setLoading] = useState(true);
+  const [joke, setJoke] = useState("");
+
+  const fetchJoke = async () => {
+    try {
+      const res = await fetch("https://icanhazdadjoke.com/", {
+        headers: {
+          Accept: "application/json",
+        },
+      });
+      const data = await res.json();
+      setJoke(data.joke);
+    } catch (err) {
+      setJoke("Failed to load joke.");
+    }
+  };
+
+  useEffect(() => {
+    console.log("Here you go anyway.");
+    setTimeout(() => {
+      fetchJoke();
+      setLoading(false);
+    }, 2000);
+  }, []);
+  return (
+    <div className="container">
+      <h2>Dad Joke:</h2>
+      {loading ? <p>Loading...</p> : <p className="joke">{joke}</p>}
+    </div>
+  );
+};
 export default JokePage;
