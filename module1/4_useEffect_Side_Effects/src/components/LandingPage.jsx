a05e321cacf9e925563eb34f0654b3560424d104
Subed <subedshahxi@gmail.com>
2025-06-17 14:58:43 +0545

    added Landing page

--- module1/4_useEffect_Side_Effects/src/components/LandingPage.jsx
+++ module1/4_useEffect_Side_Effects/src/components/LandingPage.jsx
@@ -1,5 +1,17 @@
-function LandingPage() {
-  return <div>landing page</div>;
-}
+import { useNavigate } from "react-router-dom";
+
+const LandingPage = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="container">
+      <h1>Do you want a random joke?</h1>
+      <div className="button-group">
+        <button onClick={() => navigate("/joke")}>Yes</button>
+        <button onClick={() => navigate("/joke")}>No</button>
+      </div>
+    </div>
+  );
+};
 
 export default LandingPage;
