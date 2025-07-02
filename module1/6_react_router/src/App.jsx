4ca937bfab35207cc4a7197fdf931902e0e4bdb0
Subed <subedshahxi@gmail.com>
2025-07-02 11:29:35 +0545

    simple pages using <Route> and <Link> components

--- module1/6_react_router/src/App.jsx
+++ module1/6_react_router/src/App.jsx
@@ -1,7 +1,19 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Home from "./pages/Home";
 
 function App() {
-  return <div>React Router</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Navigate replace to="home" />} />
+        <Route path="home" element={<Home />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
