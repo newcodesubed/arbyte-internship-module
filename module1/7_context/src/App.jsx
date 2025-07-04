552dc372a5e08a2408213f99610eda792623378f
Subed <subedshahxi@gmail.com>
2025-07-04 12:54:58 +0545

    app added

--- module1/7_context/src/App.jsx
+++ module1/7_context/src/App.jsx
@@ -1,9 +1,18 @@
-function App() {
-  return (
-    <div>
-      hello
-    </div>
-  )
-}
+import React, { useState } from "react";
+import { AppProviders } from "./contexts/AppProviders";
+import LoginPage from "./pages/LoginPage";
+import HomePage from "./pages/HomePage";
 
-export default App
+export default function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  return (
+    <AppProviders>
+      {isLoggedIn ? (
+        <HomePage onLogout={() => setIsLoggedIn(false)} />
+      ) : (
+        <LoginPage onLogin={() => setIsLoggedIn(true)} />
+      )}
+    </AppProviders>
+  );
+}
