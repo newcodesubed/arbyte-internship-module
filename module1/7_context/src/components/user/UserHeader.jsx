a8e111419fb8771343337b7ff3a3ecdd5be7e8bc
Subed <subedshahxi@gmail.com>
2025-07-04 12:58:05 +0545

    feat:userheader added

---
+++ module1/7_context/src/components/user/UserHeader.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { useUser } from "../../contexts/UserContext";
+
+export default function UserHeader({ onLogout }) {
+  const { user } = useUser();
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        border: "1px solid gray",
+        padding: "0.5rem",
+        borderRadius: "4px",
+      }}
+    >
+      <div
+        style={{
+          width: 40,
+          height: 40,
+          borderRadius: "50%",
+          backgroundColor: "lightgray",
+          marginRight: "1rem",
+        }}
+      ></div>
+      <div>
+        <div>{user.name}</div>
+        <div>{user.email}</div>
+      </div>
+      <button style={{ marginLeft: "auto" }} onClick={onLogout}>
+        Logout
+      </button>
+    </div>
+  );
+}
