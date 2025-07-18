098eb1d848c5d84890b469daaca9f3972c27a10b
Subed <subedshahxi@gmail.com>
2025-07-18 23:12:12 +0545

    refact togglesound

--- module1/8_custom_hooks/src/ToggleSounds.jsx
+++ module1/8_custom_hooks/src/ToggleSounds.jsx
@@ -1,17 +1,12 @@
 import { memo } from "react";
-import { useToggle } from "./hooks/useToggle";
 
 function ToggleSounds({ allowSound, setAllowSound }) {
-  const [isOn, toggle] = useToggle(allowSound);
-
-  const handleClick = () => {
-    toggle();
-    setAllowSound(!isOn);
-  };
-
   return (
-    <button className="btn-sound" onClick={handleClick}>
-      {isOn ? "🔈" : "🔇"}
+    <button
+      className="btn-sound"
+      onClick={() => setAllowSound((allow) => !allow)}
+    >
+      {allowSound ? "🔈" : "🔇"}
     </button>
   );
 }
