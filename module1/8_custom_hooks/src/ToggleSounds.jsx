ef602e9076c1f58de5528548fa8648b1f1c04ff4
Subed <subedshahxi@gmail.com>
2025-07-18 22:52:33 +0545

    added togglesound using hook

--- module1/8_custom_hooks/src/ToggleSounds.jsx
+++ module1/8_custom_hooks/src/ToggleSounds.jsx
@@ -1,5 +1,19 @@
-function ToggleSounds() {
-  return <div></div>;
+import { memo } from "react";
+import { useToggle } from "./hooks/useToggle";
+
+function ToggleSounds({ allowSound, setAllowSound }) {
+  const [isOn, toggle] = useToggle(allowSound);
+
+  const handleClick = () => {
+    toggle();
+    setAllowSound(!isOn);
+  };
+
+  return (
+    <button className="btn-sound" onClick={handleClick}>
+      {isOn ? "🔈" : "🔇"}
+    </button>
+  );
 }
 
-export default ToggleSounds;
+export default memo(ToggleSounds);
