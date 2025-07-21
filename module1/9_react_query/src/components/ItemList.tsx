701146472562e07d33ff8208cd2efffad0201c42
Subed <subedshahxi@gmail.com>
2025-07-22 01:18:45 +0545

    feat: item list

--- module1/9_react_query/src/components/ItemList.tsx
+++ module1/9_react_query/src/components/ItemList.tsx
@@ -1,3 +1,19 @@
-export default function ItemList() {
-  return <div>List</div>;
-}
+import { useFilterStore } from "../stores/filterStore";
+
+export const ItemList = () => {
+  const items = useFilterStore((s) => s.filteredItems());
+  const count = useFilterStore((s) => s.itemCount());
+
+  return (
+    <div>
+      <h3>Items ({count})</h3>
+      <ul>
+        {items.map((item) => (
+          <li key={item.id}>
+            {item.name} - {item.category} - {item.color}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
