e346a898b3ef2becf1c8a7920efeaa08f5016cd7
Subed <subedshahxi@gmail.com>
2025-07-22 00:35:45 +0545

    simulated success/failure

--- module1/9_react_query/src/components/AddItemForm.tsx
+++ module1/9_react_query/src/components/AddItemForm.tsx
@@ -1,3 +1,43 @@
-export default function AddItemForm() {
-  return <div>Form</div>;
-}
+import { useItemStore } from "../stores/itemStore";
+import { useState } from "react";
+import { toast } from "react-toastify";
+import { nanoid } from "nanoid";
+
+export const AddItemForm = () => {
+  const addItem = useItemStore((s) => s.addItem);
+  const removeItem = useItemStore((s) => s.removeItem);
+  const [name, setName] = useState("");
+
+  const handleSubmit = () => {
+    const id = nanoid();
+    const item = {
+      id,
+      name,
+      category: "Monitor",
+      color: "Black",
+    };
+
+    addItem(item);
+
+    setTimeout(() => {
+      const success = Math.random() > 0.2; // 80% success
+      if (success) {
+        toast.success("Item added successfully!");
+      } else {
+        removeItem(id);
+        toast.error("Failed to add item.");
+      }
+    }, 1000);
+  };
+
+  return (
+    <div>
+      <input
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        placeholder="Item name"
+      />
+      <button onClick={handleSubmit}>Add</button>
+    </div>
+  );
+};
