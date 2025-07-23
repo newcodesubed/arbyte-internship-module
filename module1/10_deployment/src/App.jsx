1c6f677ce339d771d0facef8e91bfdc8dfdafd30
Subed <subedshahxi@gmail.com>
2025-07-23 12:59:18 +0545

    initial commit

---
+++ module1/10_deployment/src/App.jsx
@@ -0,0 +1,59 @@
+function App() {
+  return (
+    <>
+      <header>
+        <a href="#" class="logo">
+          Subed
+        </a>
+        <nav>
+          <a href="#" class="active">
+            Home
+          </a>
+          <a href="#">Service</a>
+          <a href="#">Skill</a>
+          <a href="#">Education</a>
+          <a href="#">Experience</a>
+          <a href="#">Contact</a>
+        </nav>
+      </header>
+      <section class="home">
+        <div class="home-img">
+          <img src="download.jpeg" alt="userImage" />
+        </div>
+        <div class="home-content">
+          <h1>
+            Hi,It's <span>Subed</span>
+          </h1>
+          <h3 class="typing-text">
+            I'm a <span></span>
+          </h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt
+            qui a porro hic, error aut nulla consequuntur nam vitae. Eos
+            dignissimos veritatis dolorum alias? Temporibus ratione aliquam aut
+            ex quo?
+          </p>
+          <div class="social-icons">
+            <a href="#">
+              <i class="fa fa-linkedin" aria-hidden="true"></i>
+            </a>
+            <a href="#">
+              <i class="fa fa-github" aria-hidden="true"></i>
+            </a>
+            <a href="#">
+              <i class="fa fa-twitter" aria-hidden="true"></i>
+            </a>
+            <a href="#">
+              <i class="fa fa-instagram" aria-hidden="true"></i>
+            </a>
+          </div>
+          <a href="#" class="btn">
+            Hire me
+          </a>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default App;
