c822ad9ea158138e9be1bfef802e3ad54f0b7689
Subed <subedshahxi@gmail.com>
2025-07-27 23:56:00 +0545

    feat: route

--- module1/6_react_router/src/App.jsx
+++ module1/6_react_router/src/App.jsx
@@ -1,5 +1,31 @@
-function App() {
-  return <h1>HEllo</h1>;
-}
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import ProtectedRoute from "./components/ProtectedRoute";
+import AdminRoute from "./components/AdminRoute";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import DashboardUsers from "./pages/DashboardUsers ";
+import DashboardSettings from "./pages/DashboardSettings ";
+import AddProduct from "./pages/AddProduct ";
 
-export default App;
+export default function App() {
+  <Router>
+    <div className="p-4 flex gap-4 bg-gray-200">
+      <Link to="/">Home</Link>
+      <Link to="/login">Login</Link>
+      <Link to="/register">Register</Link>
+      <Link to="/dashboard">Dashboard</Link>
+      <Link to="/add-product">Add Product</Link>
+      {auth.isAuthenticated && (
+        <button
+          onClick={logout}
+          className="ml-auto bg-red-500 text-white px-2 rounded"
+        >
+          Logout ({auth.role})
+        </button>
+      )}
+    </div>
+  </Router>;
+}
