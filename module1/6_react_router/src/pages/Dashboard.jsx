649085abfcd7cd90bf3550bd9a88cd6a0b648029
Subed <subedshahxi@gmail.com>
2025-07-27 23:48:37 +0545

    feat: post dashboard

---
+++ module1/6_react_router/src/pages/Dashboard.jsx
@@ -0,0 +1,18 @@
+import { Outlet, Link } from "react-router-dom";
+
+export default function Dashboard() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold">Admin Dashboard</h1>
+      <div className="flex gap-4 mt-2">
+        <Link to="users" className="text-blue-500">
+          Users
+        </Link>
+        <Link to="settings" className="text-blue-500">
+          Settings
+        </Link>
+      </div>
+      <Outlet />
+    </div>
+  );
+}
