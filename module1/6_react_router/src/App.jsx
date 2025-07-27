1155c77b2829f1770e70d40c66e1c6d30bdece9a
Subed <subedshahxi@gmail.com>
2025-07-27 23:39:40 +0545

    inital commit

--- module1/6_react_router/src/App.jsx
+++ module1/6_react_router/src/App.jsx
@@ -1,37 +1,5 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Home from "./pages/Home";
-import Layout from "./pages/Layout";
-import ProductDetail from "./pages/ProductDetail";
-import PageNotFound from "./pages/PageNotFound";
-import Product from "./pages/Product";
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 0, // 1
-    },
-  },
-});
-
 function App() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="login" element={<Login />} />
-            <Route path="register" element={<Register />} />
-            <Route path="products" element={<Product />} />
-            <Route path="/products/:productId" element={<ProductDetail />} />
-          </Route>
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </QueryClientProvider>
-  );
+  return <h1>HEllo</h1>;
 }
 
 export default App;
