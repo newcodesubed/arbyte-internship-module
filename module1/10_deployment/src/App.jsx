4cd474bebba62a49c35866a4c481049a94ab6cbd
newcodesubed <subedshahxi@gmail.com>
2025-08-05 15:09:32 +0545

    update app

--- module1/10_deployment/src/App.jsx
+++ module1/10_deployment/src/App.jsx
@@ -1,57 +1,27 @@
+import React, { Suspense, lazy } from 'react';
+import Header from './components/Header';
+
+const Home = lazy(() => import('./components/Home'));
+const Services = lazy(() => import('./components/Services'));
+const Skills = lazy(() => import('./components/Skills'));
+const Education = lazy(() => import('./components/Education'));
+const Experience = lazy(() => import('./components/Experience'));
+const Contact = lazy(() => import('./components/Contact'));
+
 function App() {
   return (
     <>
-      <header>
-        <a href="#" class="logo">
-          Subed
-        </a>
-        <nav>
-          <a href="#" class="active">
-            Home
-          </a>
-          <a href="#">Service</a>
-          <a href="#">Skill</a>
-          <a href="#">Education</a>
-          <a href="#">Experience</a>
-          <a href="#">Contact</a>
-        </nav>
-      </header>
-      <section class="home">
-        <div class="home-img">
-          <img src="download.jpeg" alt="userImage" />
-        </div>
-        <div class="home-content">
-          <h1>
-            Hi,It's <span>Subed</span>
-          </h1>
-          <h3 class="typing-text">
-            I'm a <span></span>
-          </h3>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt
-            qui a porro hic, error aut nulla consequuntur nam vitae. Eos
-            dignissimos veritatis dolorum alias? Temporibus ratione aliquam aut
-            ex quo?
-          </p>
-          <div class="social-icons">
-            <a href="#">
-              <i class="fa fa-linkedin" aria-hidden="true"></i>
-            </a>
-            <a href="#">
-              <i class="fa fa-github" aria-hidden="true"></i>
-            </a>
-            <a href="#">
-              <i class="fa fa-twitter" aria-hidden="true"></i>
-            </a>
-            <a href="#">
-              <i class="fa fa-instagram" aria-hidden="true"></i>
-            </a>
-          </div>
-          <a href="#" class="btn">
-            Hire me
-          </a>
-        </div>
-      </section>
+      <Header />
+      <Suspense fallback={<div className="text-center text-white mt-10">Loading...</div>}>
+        <main>
+          <Home />
+          <Services />
+          <Skills />
+          <Education />
+          <Experience />
+          <Contact />
+        </main>
+      </Suspense>
     </>
   );
 }
