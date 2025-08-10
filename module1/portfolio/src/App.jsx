3eba38139d42d6a4f55a2a0848f1a076335eb873
newcodesubed <subedshahxi@gmail.com>
2025-08-10 15:14:34 +0545

    added hero section

--- module1/portfolio/src/App.jsx
+++ module1/portfolio/src/App.jsx
@@ -1,14 +1,16 @@
+import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
 
 function App() {
   return (
     <div className="overflow-x-hidden text-stone-300 antialiased">
       <div className="fixed inset-0 -z-10">
-        <div class="absolute top-0 z-[-2] h-screen w-screen bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]"></div>
+        <div class="absolute top-0 z-[-2] h-screen w-screen bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#000000_1px)] bg-[size:20px_20px]"></div>
       </div>
 
       <div className="container mx-auto px-8">
         <Navbar />
+        <Hero />
       </div>
       </div>
   )
