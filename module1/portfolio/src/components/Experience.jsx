658352ed4488ad6c480043fff405b4d07174ae17
newcodesubed <subedshahxi@gmail.com>
2025-08-10 18:09:40 +0545

    motion addded in exprience

--- module1/portfolio/src/components/Experience.jsx
+++ module1/portfolio/src/components/Experience.jsx
@@ -1,19 +1,32 @@
 import {EXPERIENCES} from '../constants';
+import {motion} from 'framer-motion';
 function Experience() {
   return (
     <div className='pb-4'>
-        <h2 className='my-20 text-center text-4xl'>
+        <motion.h2 
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: -100 }}
+        transition={{ duration: 0.5 }}
+        className='my-20 text-center text-4xl'>
             Experience
-        </h2>
+        </motion.h2>
         <div>
             {EXPERIENCES.map((exp, index) => (
                 <div key={index} className='mb-8 flex felx-wrap  lg:justify-center '>
-                    <div className='w-full lg:w-1/4'>
+                    <motion.div
+                    whileInView={{ opacity: 1, x: 0 }}
+                    initial={{ opacity: 0, x: -100 }}
+                    transition={{duration:1}}
+                    className='w-full lg:w-1/4'>
                         <p className='mb-2 text-sm text-stone-400'>
                             {exp.year}
                         </p>
-                    </div>
-                <div className='w-full lg:w-3/4 max-w-xl'>
+                    </motion.div>
+                <motion.div
+                whileInView={{ opacity: 1, x: 0 }}
+                    initial={{ opacity: 0, x: 100 }}
+                    transition={{duration:1}}
+                className='w-full lg:w-3/4 max-w-xl'>
                     <h3 className='mb-2  font-semibold '>
                         {exp.role} -{" "}
                         <span className='mb-4 text-stone-400'>
@@ -28,7 +41,7 @@ function Experience() {
                             {tech}
                         </span>
                     ))}
-                </div>
+                </motion.div>
 
                 </div>
             ))}
