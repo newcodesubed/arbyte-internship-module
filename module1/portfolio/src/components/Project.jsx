d13bd2837098c052dc5ead4a88cc9ea75e0b97e1
newcodesubed <subedshahxi@gmail.com>
2025-08-10 18:04:41 +0545

    added: motion in project section

--- module1/portfolio/src/components/Project.jsx
+++ module1/portfolio/src/components/Project.jsx
@@ -1,16 +1,30 @@
 import {PROJECTS} from '../constants'
+import {motion} from 'framer-motion'
+
 function Project() {
   return (
     <div className="pb-4">
 
-    <h2 className='my-20 text-center text-4xl'> Project</h2>
+    <motion.h2 
+    whileInView={{opacity: 1, y: 0}}
+    initial={{opacity: 0, y: -100}}
+    transition={{duration: 0.5}}
+    className='my-20 text-center text-4xl'> Project</motion.h2>
     <div>
         {PROJECTS.map((project, index) => (
             <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
-                <div className="w-full lg:w-1/2">
+                <motion.div
+                whileInView={{opacity: 1, x: 0}}
+                initial={{opacity: 0, x: -100}}
+                transition={{duration:1}}
+                className="w-full lg:w-1/2">
                     <img src={project.image} width={250} height={250} alt={project.title} className="mb-6 rounded" />
-                </div>
-                <div className='w-full max-w-xl lg:w-3/4'>
+                </motion.div>
+                <motion.div 
+                whileInView={{opacity: 1, x: 0}}
+                initial={{opacity: 0, x: 100}}
+                transition={{duration:1}}
+                className='w-full max-w-xl lg:w-3/4'>
                     <h3 className='mb-2 font-semibold text-2xl'>
                        {project.title} 
                     </h3>
@@ -22,7 +36,7 @@ function Project() {
                         {tech}
                     </span>
                 ))}
-                </div>
+                </motion.div>
 
             </div>
         ))}
